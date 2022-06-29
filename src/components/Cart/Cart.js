aee369fa4812070cac90794a5e93e17e3a8956c7
mdsohag65 <sohag92ahmed@gmail.com>
2022-06-29 21:15:06 +0600

    use conditional rendaring

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = () => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+    // conditional rendering options:
+    // 1. Element variable
+    //2. ternary operator => condition ? true : false
+    // && operator (shortHand)
+    let command;
+    if (cart.length === 0) {
+        command = <p>Please add at least one item!!</p>
+    }
+    else if (cart.length === 1) {
+        command = <p>Please add more...</p>
+    }
+    else {
+        command = <p>Thanks for adding items</p>
+    }
     return (
         <div>
-            <h3>Cart section</h3>
+            <h3>Item Selected: {cart.length}</h3>
+
+            {
+                cart.map(tShirt => <p>
+                    {tShirt.name}
+                    <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
+                </p>)
+            }
+            {cart.length === 3 && <div className='orange'>
+                <h3>Want some more??</h3>
+            </div>}
+            {command}
+            {cart.length !== 4 ? <p>Keep Adding</p> : <button>Clear All</button>} <br />
+            {cart.length === 4 && <button className='orange'>Proceed Checkout</button>}
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Cart from '../Cart/Cart';
 import useTshirts from '../../hooks/useTshirts';
+import TShirt from '../TShirt/TShirt';
 
 const Home = () => {
     const [tShirts, setTShits] = useTshirts();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (selectedItem) => {
+        const exists = cart.find(tShirt => tShirt._id === selectedItem._id);
+        if (!exists) {
+            const newCart = [...cart, selectedItem];
+            setCart(newCart);
+        }
+        else {
+            alert('Item Already Added');
+        }
+    }
+
+    const handleRemoveFromCart = (selectedItem) => {
+        const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
+        setCart(rest);
+    }
+
     return (
         <section className='container'>
             <div className="tshirt-container">
-
+                {
+                    tShirts.map(tShirt => <TShirt
+                        key={tShirt._id}
+                        tShirt={tShirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </section>
     );
